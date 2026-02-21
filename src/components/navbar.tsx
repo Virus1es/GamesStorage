9e5346fae79c13ffb12b5d761196161ebe215d38
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-02-21 23:07:01 +0300

    feat(ui): add icon to navbar and change favicon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,20 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navbar() {
     return (
-        <header className="fixed px-[1rem] py-6 top-0 min-w-screen bg-transparent shadow-lg z-1000">
-            <nav className="flex justify-between items-center text-white text-2xl font-extrabold">
-                <Link href="/public">Games Storage</Link>
+        <header className="fixed px-[1rem] py-3 top-0 min-w-screen bg-transparent shadow-lg z-1000">
+            <nav className="flex justify-between items-center text-white text-xl md:text-2xl font-extrabold">
+                <div className="flex flex-row items-center">
+                    <Image
+                        src="/icon.png"
+                        alt="Main site icon"
+                        width={50}
+                        height={50}
+                        className="w-fit"
+                    />
+                    <Link href="/">Games Storage</Link>
+                </div>
                 <ul className="flex gap-x-[1rem] me-6">
                     <li className="text-base font-bold transition duration-1000 hover:text-gray-400">
                         <Link href="#">Профиль</Link>
